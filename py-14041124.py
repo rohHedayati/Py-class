7b04289722ab088665d82eb85402b31a3ba37f7f
Roh hedayati <r.hedayati1372@gmail.com>
2026-02-13 18:02:34 +0330

    Update

--- py-14041124.py
+++ py-14041124.py
@@ -39,12 +39,37 @@ def fib_v2(n):
         f.append(f[i]+f[i+1])
     return f[-1]
 
-print(fib(6))
-print(fib_v2(6))
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n-1)
 
-f = [1,1]
-n = int(input("Enter n:"))
-for i in range(n-2):
-    f.append(f[i]+f[i+1])
-print(f)
-print(f[-1])
+# 5*4 = 5+5+5+5
+def mul(n,m):
+    if m==1:
+        return n
+    return n + mul(n,m-1)
+
+
+def div(n,m):
+    if n < m:
+        return 0
+    return 1 + div(n-m, m)
+
+
+# print(fib(6))
+# print(fib_v2(6))
+
+# ==================================================================
+# Files
+f = open("files/numbers.txt")
+# print(f.read(5))
+# print(f.read(5))
+# print(f.read(5))
+# print(f.readline())
+# print(f.readline())
+# print(f.readlines())
+
+s = "  hello  hi   \n"
+s2 = s.strip()
+print(s2)
